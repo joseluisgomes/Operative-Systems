709d1dbdfb269e9504168e25038838e88bed94e6
joseluisgomes <joslus.luis@gmail.com>
2021-10-19 23:41:37 +0100

    Exercises 3 and 4 completed

--- guia_1/mycat.c
+++ guia_1/mycat.c
@@ -6,34 +6,12 @@
 #include <fcntl.h> /* O_RDONLY, O_WRONLY, O_CREAT, O_* */
 
 int main(int argc, char const *argv[]) {
-    int fileDescriptor, bytesReaded;
-    char* c = (char *) calloc(100, sizeof(char));
+    char* c = (char *) calloc(1024, sizeof(char));
+    int bytesReaded;
 
-    fileDescriptor = open(argv[1], O_RDONLY); 
-    if (fileDescriptor < 0) {
-        perror("r1");
-        exit(1);
-    }
-
-    bytesReaded = read(fileDescriptor, c, 80); //3rd arg : sizeof(s)
-
-    /*
-        int main() {
-            int n;
-            char s[1024];
-            while((n = read(0,s,sizeof(s))) > 0) {
-                write(1, s , n);
-            }
-        }
-    */
-    if (bytesReaded < 0) {
-        perror("r2");
-        exit(1);
-    }
-
-    write(1, c, 80);
-
+    while((bytesReaded = read(0, c, sizeof(c))) > 0) 
+        write(1, c, bytesReaded);
+    
     free(c);
-    close(fileDescriptor);
     return 0;
 }
--- guia_1/readLine.c
+++ guia_1/readLine.c
@@ -4,25 +4,22 @@
 #include <sys/types.h>
 #include <unistd.h> /* chamadas ao sistema: defs e decls essenciais */
 #include <fcntl.h> /* O_RDONLY, O_WRONLY, O_CREAT, O_* */
-#define SPACE 1024 // 1 KB = 1024 Bytes
 
 ssize_t readln(int fd, char *line, size_t size);
 
 int main(int argc, char const *argv[]) {
 
     int bytesReaded;
-    char* c = (char*) calloc(SPACE, sizeof(char));
+    char* c = (char*) calloc(1024, sizeof(char));
     
-    int fileDescriptor = open(argv[1], O_RDONLY);
+    int fileDescriptor = open(argv[1], O_RDONLY, 0666);
     if (fileDescriptor < 0) {
         perror("r1");
-        fclose(fileDescriptor);
+        close(fileDescriptor);
         exit(1);
     }
 
-    bytesReaded = readln(fileDescriptor, c, SPACE);
-
-    printf("Bytes readed -> %d\n", bytesReaded);
+    bytesReaded = readln(fileDescriptor, c, 1024);
 
     free(c);
     close(fileDescriptor);
@@ -30,29 +27,8 @@ int main(int argc, char const *argv[]) {
 }
 
 ssize_t readln(int fd, char *line, size_t size) {
-    char* ln = calloc(SPACE, sizeof(char));
-    
-    int bytesReaded = read(fd, line, size);
-    if(bytesReaded < 0) {
-        perror("r1");
-        exit(1);
-    }
+    int bytesReaded;
 
-    int i = 0, i1 = 0;
-    int numLines = 1; // #lines
-    
-    while(i <strlen(line)) {
-        ln[i1] = line[i];
-        i1++;
-
-        if(line[i] == '\n') {
-            printf("%d: %s", numLines, ln);
-            ln[0] = '\0';
-            i1 = 0;
-            numLines++;
-        }
-        i++;
-    }
-    free(ln);
-    return bytesReaded;
+    while ((bytesReaded = read(fd, line, size)) > 0) 
+        write(1, line, bytesReaded);
 }
