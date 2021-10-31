3774420d8881ead2f9c88fa857d175d98e96e157
joseluisgomes <joslus.luis@gmail.com>
2021-10-31 13:00:43 +0000

    Exercise 6 initialized

--- guia_1/person.c
+++ guia_1/person.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <string.h>
 #include <unistd.h> /* chamadas ao sistema: defs e decls essenciais */
 #include <fcntl.h> /* O_RDONLY, O_WRONLY, O_CREAT, O_* */
 
+typedef struct { char name[60]; int age; } PERSON;
+
 int main(int argc, char const *argv[]) {
-    /* code */
+    
+    int fileDescriptor = open("people.bin", O_RDWR | O_CREAT, 0664);
+    char* errorMessage = NULL;
+    
+    if (fileDescriptor == -1 || argc != 4) {
+        errorMessage = fileDescriptor == -1 ? 
+            "open() failed to open the .bin file" : "Passed few arguments";
+        
+        perror(errorMessage);
+        free(errorMessage);
+        close(fileDescriptor);
+    } else {
+        int result;
+    }
+    
     return 0;
 }
