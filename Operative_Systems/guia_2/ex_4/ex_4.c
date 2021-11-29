e630715cac525a4e0074b6cbdea4984cdef548eb
joseluisgomes <joslus.luis@gmail.com>
2021-11-29 11:06:31 +0000

    Exercise 4 conclued

--- Operative_Systems/guia_2/ex_4/ex_4.c
+++ Operative_Systems/guia_2/ex_4/ex_4.c
@@ -3,36 +3,35 @@
 #include <sys/wait.h> 
 
 int main(int argc, char const *argv[]) {
+    int status;
     pid_t pid;
-    int status = 0;
-    int i;
 
-    for (i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++) {
+        pid = fork();
 
-        if (pid > 0) // Parent process
-            pid = fork();   
-        else // Child process
-            break;                
+        if (!pid) { // Child process
+            printf(
+                "Child #%d -> pid = %d -> Child's parent pid = %d\n",
+                ++i,
+                getpid(),
+                getppid()
+            );
+            _exit(++i);
+        } 
+        printf(
+            "My pid = %d, Parent's pid = %d, process's pid = %d\n", 
+            getpid(), 
+            getppid(), 
+            pid
+        );
     }
     
-    if (pid) // It's a parent
-        for (int i = 0; i < 10; i++) {
-            wait(&status);
-            
-            if (WIFEXITED(status)) // Child process ended successfully   
-                printf(
-                    "\nChild #%d terminated -> Child parent's id = %d\n", 
-                    WEXITSTATUS(status), 
-                    pid
-                );
-        }
-    else  // It's a child
-        printf(
-            "\nChild #%d pid = %d\n Child's parent pid = %d\n", 
-            i, 
-            getpid(), 
-            getppid()
-        );
+    for (int i = 0; i < 10; i++) {
+        pid = wait(&status);
 
+        if (WIFEXITED(status))
+            printf("Child #%d died.\n", pid);
+    }
+    
     return 0;
 }
