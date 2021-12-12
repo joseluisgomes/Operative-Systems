bdf67ad325016e1f80be4bb58933f13569669c96
joseluisgomes <joslus.luis@gmail.com>
2021-12-12 23:34:13 +0000

    exercise 3 concluded

--- Operative_Systems/guia_5/ex_3/ex_3.c
+++ Operative_Systems/guia_5/ex_3/ex_3.c
@@ -1,44 +1,28 @@
 #include <stdio.h>
-#include <string.h>
-#include <sys/wait.h>
 #include <unistd.h>
-#define MSGSIZE 16
 
-int main(int argc, char const *argv[]) {
-    int fileDes[2], bytesReaded, status;
-    pid_t pid;
-    char* inBuf[] = {}; // In buffer
-
-    dup2(0, fileDes[0]); // fileDes[0] is the new fd of the stdin 
-
+int main() {
+	int fileDes[2], bytesReaded;
+	char buffer[256];
+	
     if (pipe(fileDes) < 0) {
         perror("pipe function failed");
         _exit(0);
     }
-    
-    if (!(pid = fork())) { // Child process
-        close(fileDes[1]);
 
-        if ((bytesReaded = read(fileDes[0], inBuf, sizeof(inBuf))) > 0) {
-            close(fileDes[0]);
-            execvp("wc", inBuf);
-
-            perror("execvp() function failed.");
-            _exit(1);
-        } else 
-            _exit(2);
-    } else { // Parent process
-        close(fileDes[0]);
-
-        while((bytesReaded = read(0, inBuf, sizeof(inBuf))) > 0)
-            write(fileDes[1], inBuf, bytesReaded);
-        
-        close(fileDes[1]);
-        
-        pid = wait(&status);
-        if (WIFEXITED(status))
-            if (WEXITSTATUS(status) == 2)
-                printf("\nChild -> pid = %d, free.\n", pid);
-    }
-    return 0;
+    if (fork()) { // Parent process
+		close(fileDes[1]);
+		close(0);
+		
+        dup(fileDes[0]);
+		execlp("wc","wc",NULL);
+	} else { // Child process
+		close(fileDes[0]);
+		
+        while ((bytesReaded = read(0,buffer,sizeof(buffer))) > 0) 
+			write(fileDes[1], buffer, bytesReaded);
+		
+		wait(NULL);
+	}
+	return 0;
 }
